dc1842e6b1bf0a7050b8baa2792e2ca2880f8338
msverker <s216143@dtu.dk>
2025-01-16 10:56:27 +0100

    data.py updated for proper setup of DataLoader

--- src/pokemon_ddpm/data.py
+++ src/pokemon_ddpm/data.py
@@ -5,55 +5,58 @@ from typing import Any
 
 import torch
 from PIL import Image
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import matplotlib.pyplot as plt
 
 
 class Pokemon(Dataset):
     """Pokémon sprite dataset."""
 
-    def __init__(self, raw_data_path: Path, transform: Any = None) -> None:
-        self.data_path = raw_data_path
+    def __init__(self, data_path: Path, transform: Any = None) -> None:
+        self.image_paths = list(glob.glob(os.path.join(data_path, '*.jpg')))
         self.transform = transform
 
     def __len__(self) -> int:
         """Return the length of the dataset."""
-        return len(self.images)
+        return len(self.image_paths)
 
     def __getitem__(self, index: int):
         """Return a given sample from the dataset."""
-        image = self.images[index]
-
+        img_path = self.image_paths[index]
+        image = Image.open(img_path)
         if self.transform:
             image = self.transform(image)
-
         return image
 
-    def preprocess(self, raw_data_path: Path, preprocessed_path: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
-        os.makedirs(preprocessed_path, exist_ok=True)
-
-        transform = transforms.Compose(
-            [
-                transforms.Resize((32, 32)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            ]
-        )
-        images = []
-
-        for raw_path in glob.glob(f"{raw_data_path}/*.jpg"):
-            with Image.open(raw_path) as img:
-                tensor = transform(img)
-                images.append(tensor)
-
-        images = torch.stack(images)
-        os.makedirs(preprocessed_path, exist_ok=True)
-        torch.save(images, f"{preprocessed_path}/images.pt")
 
 
-if __name__ == "__main__":
-    raw_data_path = Path("data/raw")
-    preprocessed_path = Path("data/processed")
-    Pokemon = Pokemon(raw_data_path, transform=None)
-    Pokemon.preprocess(raw_data_path, preprocessed_path)
+# if __name__ == "__main__":
+
+#     transform = transforms.Compose(
+#         [
+#             transforms.Resize((32, 32)),
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+#         ]
+#     )
+
+#     images = Pokemon("data/pokemon_jpg", transform=transform)
+#     train_dataloader = DataLoader(images, batch_size=32, shuffle=True)
+
+#     image_features = next(iter(train_dataloader))
+#     img = image_features[0]
+#     print(img.shape)
+#     plt.imshow(img.permute(1, 2, 0))
+#     plt.show()
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     raw_data_path = Path("data/raw")
+#     preprocessed_path = Path("data/processed")
+#     Pokemon = Pokemon(raw_data_path, transform=None)
+#     Pokemon.preprocess(raw_data_path, preprocessed_path)
